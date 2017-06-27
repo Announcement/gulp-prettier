2edbf0b6b1a85c5423b6b6b92e53d17e463ea5a5
Jacob Francis Powers <xxv9xx@gmail.com>
2017-06-27 08:49:21 -0500

    limit options and save results

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
 const through = require('through2')
 const prettier = require('prettier')
+const select = require('./select')
+
+// import gutil from 'gulp-util'
+// import merge from 'merge'
+// import sourcemap from 'vinyl-sourcemaps-apply'
+
+// const PluginError = gutil.PluginError
 
 module.exports = function (options) {
+  const possible = select([
+    'printWidth',
+    'tabWidth',
+    'useTabs',
+    'semi',
+    'singleQuote',
+    'trailingComma',
+    'bracketSpacing',
+    'jsxBracketSameLine',
+    'rangeStart',
+    'rangeEnd',
+    'parser',
+    'filepath'
+  ])
+
   function transform (file, encoding, callback) {
     let contents = file.contents
-    let result = prettier.format(contents, options)
+    let result = prettier.format(contents, possible(options))
+
+    file.contents = result
 
     callback(null, file)
   }
--- source.js
+++ source.js
@@ -1,5 +1,6 @@
 import through from 'through2'
 import prettier from 'prettier'
+import select from './select'
 
 // import gutil from 'gulp-util'
 // import merge from 'merge'
@@ -8,9 +9,26 @@ import prettier from 'prettier'
 // const PluginError = gutil.PluginError
 
 export default function (options) {
+  const possible = select([
+    'printWidth',
+    'tabWidth',
+    'useTabs',
+    'semi',
+    'singleQuote',
+    'trailingComma',
+    'bracketSpacing',
+    'jsxBracketSameLine',
+    'rangeStart',
+    'rangeEnd',
+    'parser',
+    'filepath'
+  ])
+
   function transform (file, encoding, callback) {
     let contents = file.contents
-    let result = prettier.format(contents, options)
+    let result = prettier.format(contents, possible(options))
+
+    file.contents = result
 
     callback(null, file)
   }
