b3d90deebcf3bbc12d64ad192d1ad8331788397a
Jacob Francis Powers <xxv9xx@gmail.com>
2017-06-28 02:40:15 -0500

    re-enable options and remove Try-Catch statements

--- index.js
+++ index.js
@@ -1,80 +1,35 @@
 var through = require('through2')
 var prettier = require('prettier')
-var {PluginError} = require('gulp-util')
-// var select = require('./select')
-
-// import gutil from 'gulp-util'
-// import merge from 'merge'
-// import sourcemap from 'vinyl-sourcemaps-apply'
-
-// const PluginError = gutil.PluginError
+var select = require('./select')
 
 module.exports = function (configuration) {
-  // const possible = select([
-  //   'printWidth',
-  //   'tabWidth',
-  //   'useTabs',
-  //   'semi',
-  //   'singleQuote',
-  //   'trailingComma',
-  //   'bracketSpacing',
-  //   'jsxBracketSameLine',
-  //   'rangeStart',
-  //   'rangeEnd',
-  //   'parser',
-  //   'filepath'
-  // ])
-
-  function message (exception, string) {
-    let fromException = () => exception.message || exception.name
-    return fromException.message || string
-  }
+  const possible = select([
+    'printWidth',
+    'tabWidth',
+    'useTabs',
+    'semi',
+    'singleQuote',
+    'trailingComma',
+    'bracketSpacing',
+    'jsxBracketSameLine',
+    'rangeStart',
+    'rangeEnd',
+    'parser',
+    'filepath'
+  ])
 
   function transformFunction (chunk, encoding, callback) {
     var contents
     var results
-    // var options
+    var options
 
-    contents = chunk.contents
+    contents = chunk.contents.toString()
+    options = possible(configuration)
 
-    results = format()
+    results = prettier.format(contents, options)
+    chunk.contents = Buffer.from(results)
 
-    if (results && buffer()) {
-      callback(null, chunk)
-    }
-
-    function format () {
-      try {
-        results = prettier.format(contents)
-      } catch (exception) {
-        console.log(exception)
-        callback(new PluginError({
-          plugin: 'gulp-prettier',
-          message: message(exception, 'could not prettier.format(contents)')
-        }))
-
-        return true
-      }
-
-      return false
-    }
-
-    function buffer () {
-      try {
-        chunk.contents = Buffer.from(results)
-      } catch (exception) {
-        console.log(exception)
-        callback(new PluginError({
-          plugin: 'gulp-prettier',
-          message: message(exception, 'fail on .contents = buffer:results')
-        }))
-        return true
-      }
-
-      return false
-    }
-
-    // options = possible(configuration)
+    callback(null, chunk)
   }
 
   return through.obj(transformFunction)
