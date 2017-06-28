a0a8450f24cb8fffa6b6c037ebdaa8e9ecf781a9
Jacob Francis Powers <xxv9xx@gmail.com>
2017-06-28 02:30:12 -0500

    more debugging

--- index.js
+++ index.js
@@ -25,32 +25,56 @@ module.exports = function (configuration) {
   //   'filepath'
   // ])
 
-  function message (exception) {
+  function message (exception, string) {
     let fromException = () => exception.message || exception.name
-    return fromException.message || 'error formatting contents'
+    return fromException.message || string
   }
 
   function transformFunction (chunk, encoding, callback) {
     var contents
     var results
-    var error
     // var options
 
     contents = chunk.contents
-    // options = possible(configuration)
-    try {
-      results = prettier.format(contents)
-      chunk.contents = Buffer.from(results)
-      callback(null, chunk)
-    } catch (exception) {
-      error = new PluginError({
-        plugin: 'gulp-prettier',
-        message: message(exception)
-      })
-      callback(error)
 
+    results = format()
+
+    if (results && buffer()) {
+      callback(null, chunk)
     }
 
+    function format () {
+      try {
+        results = prettier.format(contents)
+      } catch (exception) {
+        console.log(exception)
+        callback(new PluginError({
+          plugin: 'gulp-prettier',
+          message: message(exception, 'could not prettier.format(contents)')
+        }))
+
+        return true
+      }
+
+      return false
+    }
+
+    function buffer () {
+      try {
+        chunk.contents = Buffer.from(results)
+      } catch (exception) {
+        console.log(exception)
+        callback(new PluginError({
+          plugin: 'gulp-prettier',
+          message: message(exception, 'fail on .contents = buffer:results')
+        }))
+        return true
+      }
+
+      return false
+    }
+
+    // options = possible(configuration)
   }
 
   return through.obj(transformFunction)
